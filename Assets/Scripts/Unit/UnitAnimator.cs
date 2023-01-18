2362eb59cbe6b54a762cc9973a57e552c224f6fa
Kyle Cuss <cussprogramming@gmail.com>
2023-01-18 17:45:06 -0330

    created bullet projectile vfx

--- Assets/Scripts/Unit/UnitAnimator.cs
+++ Assets/Scripts/Unit/UnitAnimator.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class UnitAnimator : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Transform bulletProjectilePrefab;
+    [SerializeField] private Transform shootPointTransform;
 
     private void Awake()
     {
@@ -29,8 +31,17 @@ public class UnitAnimator : MonoBehaviour
         animator.SetBool("IsWalking", false);
     }
 
-    private void ShootAction_OnShoot(object sende, EventArgs e)
+    private void ShootAction_OnShoot(object sende, ShootAction.OnShootEventArgs e)
     {
         animator.SetTrigger("Shoot");
+
+        Transform bulletProjectileTransform = Instantiate(bulletProjectilePrefab, shootPointTransform.position, Quaternion.identity);
+        BulletProjectile bulletProjectile = bulletProjectileTransform.GetComponent<BulletProjectile>();
+
+        Vector3 targetUnitShootAtPosition = e.targetUnit.GetWorldPosition();
+
+        targetUnitShootAtPosition.y = shootPointTransform.position.y;
+
+        bulletProjectile.Setup(targetUnitShootAtPosition);
     }
 }
